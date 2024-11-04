7253d2fe797dbce90ef765e60eb5a28237762a45
Baptiste Drillien <baptiste.drillien@live.fr>
2024-11-04 15:30:58 +0100

    feat: update all packages

--- src/app/signup/actions.ts
+++ src/app/signup/actions.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation"
 
 import { createClient } from "@/libs/supabase/server"
 
-export async function signup(formData: FormData) {
+export async function signup(formData: FormData): Promise<void> {
 	const supabase = createClient()
 
 	// type-casting here for convenience
@@ -15,9 +15,15 @@ export async function signup(formData: FormData) {
 		password: formData.get("password") as string,
 	}
 
-	const { error } = await supabase.auth.signUp(data)
+	try {
+		const { error } = await supabase.auth.signUp(data)
 
-	if (error) return error
+		if (error) {
+			throw new Error(error.message)
+		}
+	} catch (error) {
+		throw new Error("Une erreur est survenue lors de l'inscription")
+	}
 
 	revalidatePath("/", "layout")
 	redirect("/")
